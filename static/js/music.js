aff92a3dc8e92b112163e04e1e380fd67b5c8b22
2074178363 <64124820+2074178363@users.noreply.github.com>
2020-04-24 08:38:03 +0800

    Update music.js

--- static/js/music.js
+++ static/js/music.js
@@ -153,7 +153,7 @@ $(function() {
           $.ajax({
             type: 'POST',
             url: getUrl(),
-            timeout: 30000,
+            timeout: 50000,
             data: {
               input: input,
               filter: filter,
